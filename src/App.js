933f44dc66e4c9e0584d31386ac7fd47c778d696
azharie_muhammad <you@example.com>
2019-06-22 15:45:03 +0700

    handle route app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {IndexRoute, Switch} from 'react-router'
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+import {UserPage} from './pages'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <div className="container">
+                <Router>
+                <Route>
+                    <Route path="/:id" component={UserPage}/>
+                </Route>
+                </Router>
+            </div>
+        </div>
+    );
 }
 
 export default App;
